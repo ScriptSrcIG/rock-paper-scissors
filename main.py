a55073092454eea8b0ec35c8ad8074a405551927
ScriptSrcIG <ranadhruv411@gmail.com>
2025-12-29 17:15:56 +0530

    Add main.py to initiate RPS game plays

---
+++ main.py
@@ -0,0 +1,11 @@
+from RPS import player
+from RPS_game import play, quincy, abbey, kris, mrugesh
+
+play(player, quincy, 1000)
+play(player, abbey, 1000)
+play(player, kris, 1000)
+play(player, mrugesh, 1000)
+
+# Uncomment this line to run tests automatically
+# from test_module import test
+# test()
